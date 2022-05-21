e2af6e30b53bfc6bcc7e6b1dfbf0cfc153505447
Stuart Raynor <madstu@gmail.com>
2022-05-21 19:09:26 +0000

    Display database objects and create initial form

--- trading/models.py
+++ trading/models.py
@@ -4,16 +4,15 @@ from django.db import models
 
 
 class Trade(models.Model):
-    commodity = models.CharField(max_length=50, null=False, blank=False)
+    # Set Database fields
+    commodity = models.CharField(max_length=150, null=False, blank=False)
     price = models.FloatField()
     amount = models.IntegerField()
     buy = models.BooleanField(null=False, blank=False, default=False)
 
     # Connect to UEX API and get latest commodity prices
     API_URL = "https://api.uexcorp.space/commodities"
-    data = {
-        "api_key": os.environ.get("UEX_API_KEY")
-    }
+    data = {"api_key": os.environ.get("UEX_API_KEY")}
     response = requests.get(API_URL, headers=data)
     api_response = response.json()
 
@@ -34,14 +33,15 @@ class Trade(models.Model):
         ]
 
     # Create a new list
-    newlist = []
+    commodity_data = []
     for item in api_display:
+        # Calculate the profit and round down to 2 decimal places
         item['profit'] = round(item['trade_price_sell'] - item['trade_price_buy'], 2)
 
         # Only add tradeable commodities to the new list
         if item['kind'] != 'Temporary' and item['kind'] != 'Drug':
             if item['trade_price_buy'] > 0.01 and item['trade_price_sell'] > 0.01:
-                newlist.append(item)
+                commodity_data.append(item)
 
     def __str__(self):
         return self.commodity
--- trading/views.py
+++ trading/views.py
@@ -4,7 +4,10 @@ from .models import Trade
 # Create your views here.
 
 def index(request):
+    trades = Trade.objects.all()
     context = {
-        'api': Trade.newlist
+        'api': Trade.commodity_data,
+        'com': Trade.commodity,
+        'trades': trades
     }
     return render(request, "trading/index.html", context)
