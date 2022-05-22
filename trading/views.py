7fb1f4ac27a8d36c082b6e997174d4570d1c3867
Stuart Raynor <madstu@gmail.com>
2022-05-22 18:41:15 +0000

    Only connect to API if it's been over 1 hour since last update

--- trading/views.py
+++ trading/views.py
@@ -8,31 +8,40 @@ from .models import Trade, CommodityPrice
 
 def index(request, *args, **kwargs):
 
-    t = time.time()
-    #query_results = CommodityPrice.objects.all()
+    epoch_time = time.time()
+    update_db = CommodityPrice.objects.get(code='UPDA')
+    last_update = update_db.date_modified
 
+    # Check if it's been more than 1 hour since last update
+    if epoch_time - 3600 > last_update:
+        print("Last modified:", update_db.date_modified)
+        print("Current Time:", epoch_time)
+        # Date last modified updated in DB 
+        update_db.date_modified = int(epoch_time)
+        update_db.save()
 
-    # Connect to UEX API and get latest commodity prices
-    API_URL = "https://api.uexcorp.space/commodities"
-    data = {"api_key": os.environ.get("UEX_API_KEY")}
-    response = requests.get(API_URL, headers=data)
-    api_response = response.json()
+        # Connect to UEX API and get latest commodity prices
+        API_URL = "https://api.uexcorp.space/commodities"
+        data = {"api_key": os.environ.get("UEX_API_KEY")}
+        response = requests.get(API_URL, headers=data)
+        api_response = response.json()
 
-    # Check API is working
-    if api_response['code'] == 200:
-        api_display = api_response['data']
-    else:
-        api_display = [
-            {
-                "code": "ERRR",
-                "name": f"Response Code: {api_response['code']}",
-                "kind": f"Status: {api_response['status']}",
-                "trade_price_buy": 1,
-                "trade_price_sell": 1000,
-                "date_added": 1,
-                "date_modified": 1
-            }
-        ]
+        # Check API is working
+        if api_response['code'] == 200:
+            api_display = api_response['data']
+        else:
+            api_display = [
+                {
+                    "code": "ERRR",
+                    "name": f"Response Code: {api_response['code']}",
+                    "kind": f"Status: {api_response['status']}",
+                    "trade_price_buy": 1,
+                    "trade_price_sell": 1000,
+                    "date_added": 1,
+                    "date_modified": 1
+                }
+            ]
+
 
     # Create a new list
     commodity_data = []
@@ -40,21 +49,22 @@ def index(request, *args, **kwargs):
         # Calculate the profit and round down to 2 decimal places
         item['profit'] = round(item['trade_price_sell'] - item['trade_price_buy'], 2)
 
-        CommodityPrice.objects.create(
-            code = item['code'],
-            name = item['name'],
-            kind = item['kind'],
-            trade_price_buy = item['trade_price_buy'],
-            trade_price_sell = item['trade_price_sell'],
-            date_modified = item['date_modified'],
-            profit = item['profit']
-        )
+        # Insert new commodity
+        # CommodityPrice.objects.create(
+        #     code = item['code'],
+        #     name = item['name'],
+        #     kind = item['kind'],
+        #     trade_price_buy = item['trade_price_buy'],
+        #     trade_price_sell = item['trade_price_sell'],
+        #     date_modified = item['date_modified'],
+        #     profit = item['profit']
+        # )
 
         # Only add tradeable commodities to the new list
         if item['kind'] != 'Drug':
             if item['trade_price_buy'] > 0 and item['trade_price_sell'] > 0:
                 commodity_data.append(item)
-        
+            
 
 
     trades = Trade.objects.all()
