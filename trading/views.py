80934c8d03e80597878cc235cc5f0732d40f65a6
Stuart Raynor <madstu@gmail.com>
2022-05-22 19:09:08 +0000

    Code check to see if entry exists ot needs to be created

--- trading/views.py
+++ trading/views.py
@@ -16,6 +16,7 @@ def index(request, *args, **kwargs):
     if epoch_time - 3600 > last_update:
         print("Last modified:", update_db.date_modified)
         print("Current Time:", epoch_time)
+
         # Date last modified updated in DB 
         update_db.date_modified = int(epoch_time)
         update_db.save()
@@ -29,6 +30,33 @@ def index(request, *args, **kwargs):
         # Check API is working
         if api_response['code'] == 200:
             api_display = api_response['data']
+            for item in api_display:
+                # Calculate the profit and round down to 2 decimal places
+                item['profit'] = round(item['trade_price_sell'] - item['trade_price_buy'], 2)
+
+                # Update Database
+                if CommodityPrice.objects.get(code=item['code']):
+                    # Update existing details
+                    entry = CommodityPrice.objects.get(code=item['code'])
+                    entry.code = item['code']
+                    entry.name = item['name']
+                    entry.kind = item['kind']
+                    entry.trade_price_buy = item['trade_price_buy']
+                    entry.trade_price_sell = item['trade_price_sell']
+                    entry.date_modified = item['date_modified']
+                    entry.profit = item['profit']
+                    entry.save()
+                else:
+                    # Insert new commodity
+                    CommodityPrice.objects.create(
+                        code = item['code'],
+                        name = item['name'],
+                        kind = item['kind'],
+                        trade_price_buy = item['trade_price_buy'],
+                        trade_price_sell = item['trade_price_sell'],
+                        date_modified = item['date_modified'],
+                        profit = item['profit']
+                    )
         else:
             api_display = [
                 {
@@ -43,29 +71,17 @@ def index(request, *args, **kwargs):
             ]
 
 
-    # Create a new list
+
+
+
+
+    # Create a new list from the database.....
     commodity_data = []
-    for item in api_display:
-        # Calculate the profit and round down to 2 decimal places
-        item['profit'] = round(item['trade_price_sell'] - item['trade_price_buy'], 2)
 
-        # Insert new commodity
-        # CommodityPrice.objects.create(
-        #     code = item['code'],
-        #     name = item['name'],
-        #     kind = item['kind'],
-        #     trade_price_buy = item['trade_price_buy'],
-        #     trade_price_sell = item['trade_price_sell'],
-        #     date_modified = item['date_modified'],
-        #     profit = item['profit']
-        # )
-
-        # Only add tradeable commodities to the new list
+        # Only add legal tradeable commodities to the new list
         if item['kind'] != 'Drug':
             if item['trade_price_buy'] > 0 and item['trade_price_sell'] > 0:
                 commodity_data.append(item)
-            
-
 
     trades = Trade.objects.all()
     context = {
