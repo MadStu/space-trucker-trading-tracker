1c276ea584c56ad50d33d2fc30a5f50a8ef53e4a
Stuart Raynor <madstu@gmail.com>
2022-05-22 17:33:07 +0000

    Move API logic into views Set new database table

--- trading/admin.py
+++ trading/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Trade
+from .models import Trade, CommodityPrice
 
 # Register your models here.
 admin.site.register(Trade)
+admin.site.register(CommodityPrice)
--- trading/models.py
+++ trading/models.py
@@ -1,48 +1,27 @@
-import os
-import requests
 from django.db import models
 
 
 class Trade(models.Model):
+
     # Set Database fields
     commodity = models.CharField(max_length=150, null=False, blank=False)
     price = models.FloatField()
     amount = models.IntegerField()
     buy = models.BooleanField(null=False, blank=False, default=False)
 
-    # Connect to UEX API and get latest commodity prices
-    API_URL = "https://api.uexcorp.space/commodities"
-    data = {"api_key": os.environ.get("UEX_API_KEY")}
-    response = requests.get(API_URL, headers=data)
-    api_response = response.json()
-
-    # Check API is working
-    if api_response['code'] == 200:
-        api_display = api_response['data']
-    else:
-        api_display = [
-            {
-            "code": "ERRR",
-            "name": f"Response Code: {api_response['code']}",
-            "kind": f"Status: {api_response['status']}",
-            "trade_price_buy": 1,
-            "trade_price_sell": 1000,
-            "date_added": 1,
-            "date_modified": 1
-            }
-        ]
-
-    # Create a new list
-    commodity_data = []
-    for item in api_display:
-        # Calculate the profit and round down to 2 decimal places
-        item['profit'] = round(item['trade_price_sell'] - item['trade_price_buy'], 2)
-
-        # Only add tradeable commodities to the new list
-        # if item['kind'] != 'Temporary' and item['kind'] != 'Drug':
-        if item['kind'] != 'Drug':
-            if item['trade_price_buy'] > 0.01 and item['trade_price_sell'] > 0.01:
-                commodity_data.append(item)
-
     def __str__(self):
         return self.commodity
+
+
+class CommodityPrice(models.Model):
+    # Set CommodityPrices Database fields
+    code = models.CharField(max_length=4, null=False, blank=False)
+    name = models.CharField(max_length=150, null=False, blank=False)
+    kind = models.CharField(max_length=150, null=False, blank=False)
+    trade_price_buy = models.FloatField()
+    trade_price_sell = models.FloatField()
+    date_modified = models.IntegerField()
+    profit = models.FloatField()
+
+    def __str__(self):
+        return self.name
