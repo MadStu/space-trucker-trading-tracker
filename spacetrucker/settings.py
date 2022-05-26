4b033079a2e52953074ccd0b916b40e9e3f83d1e
Stuart Raynor <madstu@gmail.com>
2022-05-26 17:20:58 +0000

    Test deployment after changing settings files

--- spacetrucker/settings.py
+++ spacetrucker/settings.py
@@ -136,7 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
