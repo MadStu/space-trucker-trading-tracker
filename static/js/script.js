6eb02419a422338f4880c44ee1b00288ed44aa1b
Stuart Raynor <madstu@gmail.com>
2022-05-27 14:20:05 +0000

    Move JS into into own static file and make the table rows clickable

---
+++ static/js/script.js
@@ -0,0 +1,39 @@
+document.getElementById('reset_profit').addEventListener('click', function (event) {
+    document.getElementById('reset_profit').submit();
+});
+
+function setFields() {
+    // Get the selection box
+    var selectBox = document.getElementById("in_commodity");
+    // Get the selected option
+    var selectedOption = selectBox.options[selectBox.selectedIndex];
+    // Get the attribute value
+    var buyValue = selectedOption.getAttribute("data-buy");
+    var sellValue = selectedOption.getAttribute("data-sell");
+    var commodityValue = selectedOption.getAttribute("value");
+    // Get the fields
+    var buyPrice = document.getElementById("form_buy_price");
+    var sellPrice = document.getElementById("form_sell_price");
+    var formBuyCommodity = document.getElementById("form_buy_commodity");
+    var formSellCommodity = document.getElementById("form_sell_commodity");
+    // Insert the values
+    buyPrice.value = buyValue;
+    sellPrice.value = sellValue;
+    formBuyCommodity.value = commodityValue;
+    formSellCommodity.value = commodityValue;
+};
+
+function setSelect(commodity, amount) {
+    // Get the fields
+    var selectBox = document.getElementById("in_commodity");
+    var buyAmount = document.getElementById("form_buy_amount");
+    var sellAmount = document.getElementById("form_sell_amount");
+    // Change the selected commodity and insert the unit amounts
+    selectBox.value = commodity;
+    buyAmount.value = amount;
+    sellAmount.value = amount;
+    // Set the price and hidden fields
+    setFields();
+};
+
+setFields()
