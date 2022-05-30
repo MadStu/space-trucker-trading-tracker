e544f0b0dc2507de2aed194fe7bbcd5b403e797c
Stuart Raynor <madstu@gmail.com>
2022-05-30 16:31:56 +0000

    Set debug to false

--- spacetrucker/settings.py
+++ spacetrucker/settings.py
@@ -17,8 +17,6 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
-development = os.environ.get('DEVELOPMENT', False)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
