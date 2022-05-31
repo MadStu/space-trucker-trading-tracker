59f0f87f0cd7be1dd6124cf723cdba75d7a9e9d1
Stuart Raynor <madstu@gmail.com>
2022-05-31 21:45:28 +0000

    Change settings

--- spacetrucker/settings.py
+++ spacetrucker/settings.py
@@ -17,6 +17,8 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+development = os.environ.get('DEVELOPMENT', False)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -29,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = development
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
