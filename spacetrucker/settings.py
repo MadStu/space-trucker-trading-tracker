e90f47769aabbe155cab80696bad2441eb947c43
root <madstu@gmail.com>
2022-06-03 21:46:26 +0000

    Update DB

--- spacetrucker/settings.py
+++ spacetrucker/settings.py
@@ -102,18 +102,18 @@ WSGI_APPLICATION = 'spacetrucker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if local_db:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# if local_db:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL)
-    }
+# else:
+DATABASES = {
+    'default': dj_database_url.parse(DATABASE_URL)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
