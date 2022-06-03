99d1ee4baf68287dd536cc6aaa62066285041006
Stuart Raynor <madstu@gmail.com>
2022-06-03 00:03:48 +0000

    Move update time to config vars

--- trading/api_call.py
+++ trading/api_call.py
@@ -13,7 +13,7 @@ def call_the_api():
     ships = False
 
     # Time since last API call 3600 = 1 hour, 21600 = 6 hours
-    time_in_seconds = 3600
+    time_in_seconds = os.environ.get("UPDATE_TIME")
 
     # Get the Date/Time in epoch format
     epoch_time = time.time()
